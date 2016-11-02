bfceb6fe3a1f52934ba3986f8447377b27236394
Tyler Corwin <tdcorwin@email.neit.edu>
2016-11-02 00:05:11 -0400

    Add files via upload

--- public_html/Lab5/jasmine/spec/DomSpec.js
+++ public_html/Lab5/jasmine/spec/DomSpec.js
@@ -1,47 +1,185 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
 
 
 function sandboxInputOnly() {
     var el;
-
+//prior to execution of function get values
     beforeEach(function () {
         el = document.createElement('input');
 //hard coded input values 
         el.setAttribute('id', 'numbers');
         el.setAttribute('value', '123');
         el.setAttribute('type', 'text');
-
+//add el to the DOM
         document.body.appendChild(el);
-
     });
-
-
+//remove el from the DOM and reset
     afterEach(function () {
         document.body.removeChild(el);
         el = null;
     });
+}
+function sandboxNumChar() {
+    var numChar;
+//prior to execution of function get values
+    beforeEach(function () {
+        numChar = document.createElement('input');
+//hard coded input values 
+        numChar.setAttribute('id', 'numChar');
+        numChar.setAttribute('value', '123abc');
+        numChar.setAttribute('type', 'text');
+//add numChar to the DOM
+        document.body.appendChild(numChar);
+    });
+//remove numChar from the DOM and reset
+    afterEach(function () {
+        document.body.removeChild(numChar);
+        numChar = null;
+    });
+}
+function sandboxSpecialChar() {
+    var spChar;
+//prior to execution of function get values
+    beforeEach(function () {
+        spChar = document.createElement('input');
+//hard coded input values 
+        spChar.setAttribute('id', 'spChar');
+        spChar.setAttribute('value', '=#$%');
+        spChar.setAttribute('type', 'text');
+//add spChar to the DOM
+        document.body.appendChild(spChar);
+    });
+//remove spChar from the DOM and reset
+    afterEach(function () {
+        document.body.removeChild(spChar);
+        numChar = null;
+    });
+}
+
+//text Decoration Overline using Class
+function txtDecsOver(textdecoration) {
+    var txtDecsOver;
+//prior to execution of function get text Decoration style
+    beforeEach(function () {
+        txtDecsOver = document.createElement(textdecoration);
+        txtDecsOver.style.textDecoration = 'overline';
+        txtDecsOver.classList.add('decClass');
+//add txtDecsOver to the DOM    
+        document.body.appendChild(txtDecsOver);
+    });
+//remove txtDecsOver from the DOM and reset  
+    afterEach(function () {
+        document.body.removeChild(txtDecsOver);
+        txtDecsOver = null;
+    });
+}
+
+//text Decoration styles using Class
+function txtDecs(textdecoration) {
+    var txtDec;
+//prior to execution of function get txtDec Style
+    beforeEach(function () {
+        txtDec = document.createElement(textdecoration);
+        txtDec.style.textDecoration = '';
+        txtDec.classList.add('decClass');
+//add txtDec to the DOM    
+        document.body.appendChild(txtDec);
+    });
+//remove txtDec from the DOM and reset
+    afterEach(function () {
+        document.body.removeChild(txtDec);
+        txtDec = null;
+    });
 
 }
 
-
+//inputOnly
 describe("getElementContent text", function () {
-
+//validate that the entry is numbers only using isNaN()
     sandboxInputOnly();
-
+    //1
     it('should return true if the input box value is only numeric', function () {
         var elemContent = getNumber('#numbers');
         expect(elemContent).toBe(true);
-        
-        
+    });
+});
+//numChar
+describe("getElementContent text", function () {
+//validate that the entry is numbers only using isNaN()
+    sandboxNumChar();
+    //1
+    it('should return false if the input box value is not all numeric', function () {
+        var elemContent = getNumber('#numChar');
+        expect(elemContent).toBe(true);
+    });
+});
+//specialChar
+describe("getElementContent text", function () {
+//validate that the entry is numbers only using isNaN()
+    sandboxSpecialChar();
+    //1
+    it('should return false if the input box value is not all numeric', function () {
+        var elemContent = getNumber('#spChar');
+        expect(elemContent).toBe(true);
+    });
+});
+
+describe("text-decoration set", function () {
+
+    txtDecsOver('div');
+
+    it('should match overline', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).toEqual('overline');
+
     });
 
-   
+    it('should not match none', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('none');
+
+    });
+
+    it('should not match underline', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('underline');
+
+    });
+
+    it('should not match line-through', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('line-through');
+
+    });
+});
 
 
+
+describe("text-decoration not set", function () {
+
+    txtDecs('div');
+
+    it('should not match overline', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('overline');
+
+    });
+
+    it('should not match none', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('none');
+
+    });
+
+    it('should not match underline', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('underline');
+
+    });
+
+    it('should not match line-through', function () {
+        var elemContent = getDecoration('div.decClass');
+        expect(elemContent).not.toEqual('line-through');
+    });
 });
--- public_html/Lab5/jasmine/src/Dom.js
+++ public_html/Lab5/jasmine/src/Dom.js
@@ -2,3 +2,8 @@ function getNumber(selector){
     var element = document.querySelector(selector);
     return  isNaN(element.Value);
 }
+
+function getDecoration(selector){
+    var element = document.querySelector(selector);
+    return element.style.textDecoration;
+}
